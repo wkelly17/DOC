34463e69a361f5e5617a35ba0b76a29a6cd202ee
linearcombination <4829djaskdfj@gmail.com>
2020-08-14 20:01:07 -0700

    Add lookup_tq_zips_for_lang and associated test function

--- resource_json_lookup.py
+++ resource_json_lookup.py
@@ -109,6 +109,16 @@ values from it using jsonpath. """
         )
         return zip_urls
 
+    def lookup_tq_zips_for_lang(self, lang: str) -> List[str]:
+        """ Return zip file URLs for translation questions (code: 'tq'). """
+        # Based on lang value you can use a lookup dictionary that
+        # returns the jsonpath to use. This is where we handle the
+        # unpredictable structure of translations.json.
+        zip_urls: List[str] = self.lookup(
+            "$[?name='{0}'].contents[?code='tq'].links[?format='zip'].url".format(lang)
+        )
+        return zip_urls
+
     def lookup_download_url(
         self,
         jsonpath: Optional[
@@ -160,6 +170,8 @@ def main() -> None:
     # test_lookup_tn_zips_for_lang(lookup_svc, "Assamese")
 
     # test_lookup_tw_zips_for_lang(lookup_svc, "Plateau Malagasy")
+
+    # test_lookup_tq_zips_for_lang(lookup_svc, "മലയാളം  (Malayalam)")
 
     # Test Abadi language
     lang: str = "Abadi"
@@ -245,6 +257,11 @@ def test_lookup_tn_zips_for_lang(lookup_svc: ResourceJsonLookup, lang: str) -> N
 def test_lookup_tw_zips_for_lang(lookup_svc: ResourceJsonLookup, lang: str) -> None:
     values: List[str] = lookup_svc.lookup_tw_zips_for_lang(lang)
     print("Translation words for lang {0}: {1}".format(lang, values))
+
+
+def test_lookup_tq_zips_for_lang(lookup_svc: ResourceJsonLookup, lang: str) -> None:
+    values: List[str] = lookup_svc.lookup_tq_zips_for_lang(lang)
+    print("Translation questions for lang {0}: {1}".format(lang, values))
 
 
 if __name__ == "__main__":
