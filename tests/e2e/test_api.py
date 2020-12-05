aa766f94ca0422a298ec7fe01d766cf848fb38b3
linearcombination <4829djaskdfj@gmail.com>
2020-12-04 21:42:57 -0800

    Temporarily enable and disable certain tests
    * Just commenting out some tests that I am not ready to run yet and
    adding simpler ones to test things end to end.

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -2,48 +2,65 @@ import pytest
 
 from . import api_client
 
-@pytest.mark.usefixtures("restart_api")
-# def test_happy_path_returns_202_and_batch_is_allocated():
-def test_happy_path_returns_ok():
-    payload = {}
-    payload["resources"] = [
-        {"lang_code": "en", "resource_type": "ulb-wa", "resource_code": "gen"},
-        {"lang_code": "en", "resource_type": "tn-wa", "resource_code": "gen"},
-        # {"lang_code": "am", "resource_type": "ulb", "resource_code": "deu"},
-        # {"lang_code": "erk-x-erakor", "resource_type": "reg", "resource_code": "eph"},
-        # {"lang_code": "ml", "resource_type": "ulb", "resource_code": "tit"},
-        # {"lang_code": "ml", "resource_type": "obs-tq", "resource_code": ""},
-        # {"lang_code": "mr", "resource_type": "udb", "resource_code": "mrk"},
-    ]
-    payload["assembly_strategy"] = "book"  # verse, chapter, book
-    r = api_client.post_document(payload)
-    assert r.ok
 
-    # r = api_client.get_allocation(orderid)
-    # assert r.ok
-    # assert r.json() == [
+# @pytest.mark.usefixtures("restart_api")
+# def test_happy_path_returns_ok():
+#     payload = {}
+#     payload["resources"] = [
+#         {"lang_code": "en", "resource_type": "ulb-wa", "resource_code": "gen"},
+#         {"lang_code": "en", "resource_type": "tn-wa", "resource_code": "gen"},
+#         # {"lang_code": "am", "resource_type": "ulb", "resource_code": "deu"},
+#         # {"lang_code": "erk-x-erakor", "resource_type": "reg", "resource_code": "eph"},
+#         # {"lang_code": "ml", "resource_type": "ulb", "resource_code": "tit"},
+#         # {"lang_code": "ml", "resource_type": "obs-tq", "resource_code": ""},
+#         # {"lang_code": "mr", "resource_type": "udb", "resource_code": "mrk"},
+#     ]
+#     payload["assembly_strategy"] = "book"  # verse, chapter, book
+#     r = api_client.post_document(payload)
+#     assert r.ok
+
+#     # r = api_client.get_allocation(orderid)
+#     # assert r.ok
+#     # assert r.json() == [
+#     #     {"sku": sku, "batchref": earlybatch},
+#     # ]
+
+
+@pytest.mark.usefixtures("restart_api")
+def test_get_language_codes_returns_ok():
+    r = api_client.get_language_codes()
+    assert r.ok
+    assert r.json()  # == [
     #     {"sku": sku, "batchref": earlybatch},
     # ]
 
 
-# @pytest.mark.usefixtures("postgres_db")
 @pytest.mark.usefixtures("restart_api")
-def test_unhappy_path_returns_400_and_error_message():
-    payload = {}
-    payload["resources"] = [
-        # {"lang_code": "en", "resource_type": "ulb-wa", "resource_code": "gen"},
-        # {"lang_code": "en", "resource_type": "tn-wa", "resource_code": "gen"},
-        # {"lang_code": "am", "resource_type": "ulb", "resource_code": "deu"},
-        {"lang_code": "erk-x-erakor", "resource_type": "reg", "resource_code": "eph"},
-        # {"lang_code": "ml", "resource_type": "ulb", "resource_code": "tit"},
-        # {"lang_code": "ml", "resource_type": "obs-tq", "resource_code": ""},
-        # {"lang_code": "mr", "resource_type": "udb", "resource_code": "mrk"},
-    ]
-    payload["assembly_strategy"] = "book"  # verse, chapter, book
-    # unknown_sku, orderid = random_sku(), random_orderid()
-    r = api_client.post_to_allocate(payload, expect_success=False)
-    assert r.status_code == 400
-    # assert r.json()["message"] == f"Invalid sku {unknown_sku}"
+def test_get_language_codes_and_names_returns_ok():
+    r = api_client.get_language_codes_and_names()
+    assert r.ok
+    assert r.json()  # == [
+    #     {"sku": sku, "batchref": earlybatch},
+    # ]
 
-    # r = api_client.get_allocation(orderid)
-    # assert r.status_code == 404
+
+# @pytest.mark.usefixtures("restart_api")
+# def test_unhappy_path_returns_400_and_error_message():
+#     payload = {}
+#     payload["resources"] = [
+#         # {"lang_code": "en", "resource_type": "ulb-wa", "resource_code": "gen"},
+#         # {"lang_code": "en", "resource_type": "tn-wa", "resource_code": "gen"},
+#         # {"lang_code": "am", "resource_type": "ulb", "resource_code": "deu"},
+#         {"lang_code": "erk-x-erakor", "resource_type": "reg", "resource_code": "eph"},
+#         # {"lang_code": "ml", "resource_type": "ulb", "resource_code": "tit"},
+#         # {"lang_code": "ml", "resource_type": "obs-tq", "resource_code": ""},
+#         # {"lang_code": "mr", "resource_type": "udb", "resource_code": "mrk"},
+#     ]
+#     payload["assembly_strategy"] = "book"  # verse, chapter, book
+#     # unknown_sku, orderid = random_sku(), random_orderid()
+#     r = api_client.post_to_allocate(payload, expect_success=False)
+#     assert r.status_code == 400
+#     # assert r.json()["message"] == f"Invalid sku {unknown_sku}"
+
+#     # r = api_client.get_allocation(orderid)
+#     # assert r.status_code == 404
