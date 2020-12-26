2e9d8c2ef8bedce8c164ed651cdeecc54c6f6f38
linearcombination <4829djaskdfj@gmail.com>
2020-12-25 16:15:15 -0800

    Split the resource_lookup module unit tests into sematically separate files
    * Easier for understanding to newcomers

---
+++ tests/unit/test_resource_lookup_api.py
@@ -0,0 +1,43 @@
+from document import config
+from document.domain import resource_lookup
+from document.domain import model
+from document.domain.resource import resource_factory
+
+
+## Test the API:
+
+
+def test_lookup() -> None:
+    assembly_strategy_kind: model.AssemblyStrategyEnum = model.AssemblyStrategyEnum.BOOK
+    resource_requests: List[model.ResourceRequest] = []
+    resource_requests.append(
+        model.ResourceRequest(
+            lang_code="en", resource_type="ulb-wa", resource_code="gen"
+        )
+    )
+    resource_requests.append(
+        model.ResourceRequest(
+            lang_code="en", resource_type="tn-wa", resource_code="gen"
+        )
+    )
+
+    resource_requests.append(
+        model.ResourceRequest(lang_code="mr", resource_type="ulb", resource_code="gen")
+    )
+
+    resource_requests.append(
+        model.ResourceRequest(
+            lang_code="erk-x-erakor", resource_type="reg", resource_code="eph"
+        )
+    )
+    document_request = model.DocumentRequest(
+        assembly_strategy_kind=assembly_strategy_kind,
+        resource_requests=resource_requests,
+    )
+
+    for resource_request in document_request.resource_requests:
+        r = resource_factory(
+            config.get_working_dir(), config.get_output_dir(), resource_request
+        )
+        r.find_location()
+        assert r._resource_url
--- tests/unit/test_resource_lookup_misc_jsonpath.py
+++ tests/unit/test_resource_lookup_misc_jsonpath.py
@@ -1,76 +1,7 @@
-from document import config
 from document.domain import resource_lookup
-from document.domain import model
-from document.domain.resource import resource_factory
 
-
-## Test the API:
-
-
-def test_lookup() -> None:
-    assembly_strategy_kind: model.AssemblyStrategyEnum = model.AssemblyStrategyEnum.BOOK
-    resource_requests: List[model.ResourceRequest] = []
-    resource_requests.append(
-        model.ResourceRequest(
-            lang_code="en", resource_type="ulb-wa", resource_code="gen"
-        )
-    )
-    resource_requests.append(
-        model.ResourceRequest(
-            lang_code="en", resource_type="tn-wa", resource_code="gen"
-        )
-    )
-
-    resource_requests.append(
-        model.ResourceRequest(lang_code="mr", resource_type="ulb", resource_code="gen")
-    )
-
-    resource_requests.append(
-        model.ResourceRequest(
-            lang_code="erk-x-erakor", resource_type="reg", resource_code="eph"
-        )
-    )
-    document_request = model.DocumentRequest(
-        assembly_strategy_kind=assembly_strategy_kind,
-        resource_requests=resource_requests,
-    )
-
-    for resource_request in document_request.resource_requests:
-        r = resource_factory(
-            config.get_working_dir(), config.get_output_dir(), resource_request
-        )
-        r.find_location()
-        assert r._resource_url
-
-
-## Test BIELHelperResourceJsonLookup service
-
-
-def test_lookup_all_language_codes() -> None:
-    lookup_svc = resource_lookup.BIELHelperResourceJsonLookup()
-    values = lookup_svc.lang_codes()
-    assert len(values) > 0
-
-
-def test_lookup_all_language_codes_and_names() -> None:
-    lookup_svc = resource_lookup.BIELHelperResourceJsonLookup()
-    values: List[Tuple[str, str]] = lookup_svc.lang_codes_and_names()
-    assert len(values) > 0
-
-
-def test_lookup_all_resource_types() -> None:
-    lookup_svc = resource_lookup.BIELHelperResourceJsonLookup()
-    values: List[str] = lookup_svc.resource_types()
-    assert len(values) > 0
-
-
-def test_lookup_all_resource_codes() -> None:
-    lookup_svc = resource_lookup.BIELHelperResourceJsonLookup()
-    values: List[str] = lookup_svc.resource_codes()
-    assert len(values) > 0
-
-
-## A few random tests exercising jsonpath
+## A few random tests exercising jsonpath of no real use for the API,
+## but just doing some jsonpath experiments.
 
 
 def test_lookup_downloads_at_reg() -> None:
