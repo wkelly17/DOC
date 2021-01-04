6fe9247bdd3d08f0a19d11d9c2866e7b6dd397d2
linearcombination <4829djaskdfj@gmail.com>
2021-01-03 17:18:11 -0800

    Add document_generator module tests
    * Add driver style test (to stdout) for document_generator that tests
    interleaving at verse level.
    * Add pytest test for document_generator that tests
    interleaving at verse level.

--- tests/test_document_generator.py
+++ tests/test_document_generator.py
@@ -40,6 +40,32 @@ def main() -> None:
     )
     doc_gen.run()
 
+    logger.info("BOOK interleaving test is complete")
+
+    assembly_strategy_kind: model.AssemblyStrategyEnum = model.AssemblyStrategyEnum.VERSE
+    resource_requests: List[model.ResourceRequest] = []
+    resource_requests.append(
+        model.ResourceRequest(
+            lang_code="en", resource_type="ulb-wa", resource_code="jud"
+        )
+    )
+    resource_requests.append(
+        model.ResourceRequest(
+            lang_code="en", resource_type="tn-wa", resource_code="jud"
+        )
+    )
+    document_request = model.DocumentRequest(
+        assembly_strategy_kind=assembly_strategy_kind,
+        resource_requests=resource_requests,
+    )
+
+    doc_gen = document_generator.DocumentGenerator(
+        document_request, config.get_working_dir(), config.get_output_dir(),
+    )
+    doc_gen.run()
+
+    logger.info("VERSE interleaving test is complete")
+
 
 if __name__ == "__main__":
     main()
--- tests/unit/test_document_generator.py
+++ tests/unit/test_document_generator.py
@@ -7,7 +7,7 @@ from document.domain import model
 from document.domain import document_generator
 
 
-def test_document_generator_for_english() -> None:
+def test_document_generator_for_english_with_interleaving_by_book() -> None:
     filename = "en-ulb-wa-eph_en-tn-wa-eph.html"
     filepath = os.path.join(config.get_output_dir(), filename)
     if os.path.isfile(filepath):
@@ -36,4 +36,36 @@ def test_document_generator_for_english() -> None:
     assert doc_gen._document_request_key
     assert os.path.isfile(
         os.path.join(config.get_working_dir(), "en-ulb-wa-eph_en-tn-wa-eph.html")
+    )
+
+
+def test_document_generator_for_english_with_interleaving_by_verse() -> None:
+    filename = "en-ulb-wa-jud_en-tn-wa-jud.html"
+    filepath = os.path.join(config.get_output_dir(), filename)
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    assembly_strategy_kind: model.AssemblyStrategyEnum = model.AssemblyStrategyEnum.VERSE
+    resource_requests: List[model.ResourceRequest] = []
+    resource_requests.append(
+        model.ResourceRequest(
+            lang_code="en", resource_type="ulb-wa", resource_code="jud"
+        )
+    )
+    resource_requests.append(
+        model.ResourceRequest(
+            lang_code="en", resource_type="tn-wa", resource_code="jud"
+        )
+    )
+    document_request = model.DocumentRequest(
+        assembly_strategy_kind=assembly_strategy_kind,
+        resource_requests=resource_requests,
+    )
+
+    doc_gen = document_generator.DocumentGenerator(
+        document_request, config.get_working_dir(), config.get_output_dir(),
+    )
+    doc_gen.run()
+    assert doc_gen._document_request_key
+    assert os.path.isfile(
+        os.path.join(config.get_working_dir(), "en-ulb-wa-jud_en-tn-wa-jud.html")
     )
