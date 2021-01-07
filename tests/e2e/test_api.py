e3936bf74574faae417cb995091278e3f471eedc
linearcombination <4829djaskdfj@gmail.com>
2021-01-07 12:57:05 -0800

    Add another test for verse level interleaving

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -4,7 +4,7 @@ from document import config
 from document.entrypoints.app import app
 
 
-def test_happy_path_returns_ok() -> None:
+def test_generate_book_interleaved_document_returns_ok() -> None:
     with TestClient(app=app, base_url=config.get_api_test_url()) as client:
         response = client.post(
             "/documents",
@@ -24,12 +24,36 @@ def test_happy_path_returns_ok() -> None:
                 ],
             },
         )
-        print(response.json())
+        # print(response.json())
         assert response.status_code == 200, response.text
 
         # assert response.json() == {
         #     "finished_document_url": "/working/temp/en-ulb-wa-eph_en-tn-wa-eph.html"
         # }
+
+
+def test_generate_verse_interleaved_document_returns_ok() -> None:
+    with TestClient(app=app, base_url=config.get_api_test_url()) as client:
+        response = client.post(
+            "/documents",
+            json={
+                "assembly_strategy_kind": "verse",
+                "resource_requests": [
+                    {
+                        "lang_code": "en",
+                        "resource_type": "ulb-wa",
+                        "resource_code": "jud",
+                    },
+                    {
+                        "lang_code": "en",
+                        "resource_type": "tn-wa",
+                        "resource_code": "jud",
+                    },
+                ],
+            },
+        )
+        # print(response.json())
+        assert response.status_code == 200, response.text
 
 
 # TODO
