43f9f85b643f9c19e997ef5b9f7b340ca511a8d6
linearcombination <4829djaskdfj@gmail.com>
2021-01-11 20:15:00 -0800

    Remove a couple obselete scripts

--- test_document_generator.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-rm -rf ./working/temp/*
-git restore working/temp/icon-tn.png
-git restore working/temp/keepme.txt
-python -m tests.test_document_generator
-# echo "Prettifying downloaded translations.json for easier investigation"
-# prettier -w working/temp/translations.json
