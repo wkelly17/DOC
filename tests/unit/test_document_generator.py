eb7d46b210f762a4bf3e5bd87219b6ac9c576c74
linearcombination <4829djaskdfj@gmail.com>
2022-01-19 11:44:22 -0800

    Add unit test for document_generator.document_request_key

--- tests/unit/test_document_generator.py
+++ tests/unit/test_document_generator.py
@@ -1,3 +1,5 @@
+import re
+
 from document.config import settings
 from document.domain import model, document_generator
 
@@ -40,3 +42,86 @@ def test_coalesce_english_tn_requests() -> None:
     # Extra tn resource request is filtered out, leaving 4 instead of
     # 5 resource requests.
     assert len(resource_requests_) == 4
+
+
+def test_document_request_key_too_long_for_semantic_result() -> None:
+    """
+    Use enough resource requests that a semantic name built from them
+    will be too long which will cause the document request key algorithm
+    to use a timestamp-based, non-semantic, name.
+    """
+    components = [
+        (
+            "bdf",
+            "reg",
+            "mat",
+        ),
+        (
+            "bdf",
+            "reg",
+            "mrk",
+        ),
+        (
+            "pt-br",
+            "ulb",
+            "mat",
+        ),
+        (
+            "pt-br",
+            "tw",
+            "mat",
+        ),
+        (
+            "pt-br",
+            "tq",
+            "mat",
+        ),
+        (
+            "pt-br",
+            "tn",
+            "mat",
+        ),
+        (
+            "pt-br",
+            "ulb",
+            "mrk",
+        ),
+        (
+            "pt-br",
+            "tw",
+            "mrk",
+        ),
+        (
+            "pt-br",
+            "tq",
+            "mrk",
+        ),
+        (
+            "pt-br",
+            "tn",
+            "mrk",
+        ),
+        ("fr", "ulb", "mat"),
+        ("fr", "tw", "mat"),
+        ("fr", "tq", "mat"),
+        ("fr", "tn", "mat"),
+        ("fr", "f10", "mat"),
+        ("fr", "ulb", "mrk"),
+        ("fr", "tw", "mrk"),
+        ("fr", "tq", "mrk"),
+        ("fr", "tn", "mrk"),
+        ("fr", "f10", "mrk"),
+    ]
+    resource_requests = [
+        model.ResourceRequest(
+            lang_code=component[0],
+            resource_type=component[1],
+            resource_code=component[2],
+        )
+        for component in components
+    ]
+    assembly_strategy_kind = model.AssemblyStrategyEnum.BOOK_LANGUAGE_ORDER
+    key = document_generator.document_request_key(
+        resource_requests, assembly_strategy_kind
+    )
+    assert re.search(r"[0-9]+_[0-9]+", key)
