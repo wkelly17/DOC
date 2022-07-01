25e13173433a3836256d0fb625ad1e6b39d2344b
linearcombination <4829djaskdfj@gmail.com>
2022-07-01 10:15:38 -0700

    Formating changes
    Prettier formatted automatically

--- frontend/src/__tests__/step.spec.ts
+++ frontend/src/__tests__/step.spec.ts
@@ -1,41 +1,49 @@
-import { tick } from 'svelte';
-import { render } from '@testing-library/svelte';
-import userEvent from '@testing-library/user-event';
+import { tick } from 'svelte'
+import { render } from '@testing-library/svelte'
+import userEvent from '@testing-library/user-event'
 
-import FauxStepForm from './utils/FauxStepForm.svelte';
+import FauxStepForm from './utils/FauxStepForm.svelte'
 
 test('component renders with inputs', async () => {
-  const { getByLabelText, getByPlaceholderText, getByText } = render(FauxStepForm, { props: { name: 'test_form' } });
+  const { getByLabelText, getByPlaceholderText, getByText } = render(FauxStepForm, {
+    props: { name: 'test_form' }
+  })
 
-  expect(getByLabelText('Test Input').attributes.getNamedItem('name')?.value).toBe('test_input');
-  expect(() => getByLabelText('Test Input 2')).toThrow();
-  expect(() => getByLabelText('Test Input 3')).toThrow();
+  expect(getByLabelText('Test Input').attributes.getNamedItem('name')?.value).toBe(
+    'test_input'
+  )
+  expect(() => getByLabelText('Test Input 2')).toThrow()
+  expect(() => getByLabelText('Test Input 3')).toThrow()
 
-  expect(() => getByText('Prev')).toThrow();
-  expect(() => getByText('Next')).not.toThrow();
-  expect(() => getByPlaceholderText('Submit')).toThrow();
+  expect(() => getByText('Prev')).toThrow()
+  expect(() => getByText('Next')).not.toThrow()
+  expect(() => getByPlaceholderText('Submit')).toThrow()
 
-  userEvent.click(getByText('Next'));
+  userEvent.click(getByText('Next'))
 
-  await tick();
+  await tick()
 
-  expect(() => getByLabelText('Test Input')).toThrow();
-  expect(getByLabelText('Test Input 2').attributes.getNamedItem('name')?.value).toBe('test_input_2');
-  expect(() => getByLabelText('Test Input 3')).toThrow();
+  expect(() => getByLabelText('Test Input')).toThrow()
+  expect(getByLabelText('Test Input 2').attributes.getNamedItem('name')?.value).toBe(
+    'test_input_2'
+  )
+  expect(() => getByLabelText('Test Input 3')).toThrow()
 
-  expect(() => getByText('Prev')).not.toThrow();
-  expect(() => getByText('Next')).not.toThrow();
-  expect(() => getByPlaceholderText('Submit')).toThrow();
+  expect(() => getByText('Prev')).not.toThrow()
+  expect(() => getByText('Next')).not.toThrow()
+  expect(() => getByPlaceholderText('Submit')).toThrow()
 
-  userEvent.click(getByText('Next'));
+  userEvent.click(getByText('Next'))
 
-  await tick();
+  await tick()
 
-  expect(() => getByLabelText('Test Input')).toThrow();
-  expect(() => getByLabelText('Test Input 2')).toThrow();
-  expect(getByLabelText('Test Input 3').attributes.getNamedItem('name')?.value).toBe('test_input_3');
+  expect(() => getByLabelText('Test Input')).toThrow()
+  expect(() => getByLabelText('Test Input 2')).toThrow()
+  expect(getByLabelText('Test Input 3').attributes.getNamedItem('name')?.value).toBe(
+    'test_input_3'
+  )
 
-  expect(() => getByText('Prev')).not.toThrow();
-  expect(() => getByText('Next')).toThrow();
-  expect(() => getByPlaceholderText('Submit')).not.toThrow();
-});
+  expect(() => getByText('Prev')).not.toThrow()
+  expect(() => getByText('Next')).toThrow()
+  expect(() => getByPlaceholderText('Submit')).not.toThrow()
+})
