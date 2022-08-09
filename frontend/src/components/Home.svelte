02aab586eedb3d6418d3f695f04845d8bb39e948
linearcombination <4829djaskdfj@gmail.com>
2022-08-08 20:01:16 -0700

    Add resource types button to Home spa page

--- frontend/src/components/Home.svelte
+++ frontend/src/components/Home.svelte
@@ -64,14 +64,30 @@
         <span class="text-grey-200 text-sm capitalize">{books}</span>
       </div>
     {/if}
-    <!-- {#if $lang0NameAndCode} -->
+  </li>
+  <li class="bg-white border-b-4 border-orange-500 p-2">
+    <button
+      class="inline-flex items-center w-full justify-between text-xl text-orange-500 font-bold py-2 px-4 rounded"
+      on:click={() => push('#/resource_types')}
+    >
+      <span>Resource types</span>
+      <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M0 7.00002V9.00002H12L6.5 14.5L7.92 15.92L15.84 8.00002L7.92 0.0800171L6.5 1.50002L12 7.00002H0Z"
+          fill="#140E08"
+        />
+      </svg>
+    </button>
+    <!-- {#if $resourceTypesStore} -->
     <!--   <div> -->
-    <!--     <span class="text-grey-200 text-sm capitalize" -->
-    <!--       >{lang0Name}{#if $lang1NameAndCode}, -->
-    <!--         {lang1Name}{/if}</span -->
-    <!--     > -->
+    <!--     <span class="text-grey-200 text-sm capitalize">{resourceTypes}</span> -->
     <!--   </div> -->
     <!-- {/if} -->
   </li>
-  <li><a href="#/resource_types">Resource types</a></li>
 </ul>
