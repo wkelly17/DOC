0ff7fb5a5b4e67174c45b745434334b19dcbc397
linearcombination <4829djaskdfj@gmail.com>
2022-10-01 10:27:40 -0700

    Make ProgressIndicator component only due text indicator
    Cleaner code. Not currently used, but because the indeterminate
    progress indicator that we are using relies on webkit css frames, it
    may prove unreliable for a significant number of users (due to
    browser) such that we revert to this progress indicator.

--- frontend/src/components/ProgressIndicator.svelte
+++ frontend/src/components/ProgressIndicator.svelte
@@ -3,12 +3,6 @@
   let text: string = ''
   export let labelString: string = 'Loading'
   export let parClass: string = ''
-  export let graphicStyle: boolean = false
-  let graphicClassDefault: string = 'progress'
-  export let graphicClass: string = 'w-56'
-  let graphicClasses: string = `${graphicClassDefault} ${graphicClass}`
-  export let graphicValue: string = '30'
-  export let graphicMax: string = '100'
 
   async function loop() {
     if (text.length === 3) text = ''
@@ -22,8 +16,10 @@
   })
 </script>
 
-{#if graphicStyle}
-  <progress class={graphicClasses} value={graphicValue} max={graphicMax} />
-{:else}
-  <p class={parClass}>{labelString}{text}</p>
-{/if}
+<p class={parClass}>{labelString}{text}</p>
+
+<!-- Usage example: -->
+<!-- <ProgressIndicator -->
+<!--   parClass="text-center text-secondary-content" -->
+<!--   labelString="Generating document" -->
+<!-- /> -->
