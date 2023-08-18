ad7cf57439f23a73ede578ce1f5b20b1dfa0b264
linearcombination <4829djaskdfj@gmail.com>
2023-08-17 17:06:10 -0700

    Basic layout sorted now

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -5,10 +5,8 @@
   let open = false
 </script>
 
-<main data-theme="wa" class="bg-white">
-  <!-- <div class="container mx-auto w-full lg:w-3/5 px-2 pt-2 mt-2"> -->
-  <!-- <div class="container h-screen w-full flex flex-col overflow-hidden"> -->
-  <div class="h-screen w-full flex flex-col overflow-hidden">
+<div data-theme="wa" class="bg-white">
+  <div class="h-screen flex flex-col">
     <Router {routes} />
   </div>
-</main>
+</div>
