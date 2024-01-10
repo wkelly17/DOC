7dd1af2bad5f87e38534b6443a96d675d92ea206
linearcombination <4829djaskdfj@gmail.com>
2024-01-10 09:26:38 -0800

    Add new UI test

--- frontend/tests/e2e/e2e.spec.ts
+++ frontend/tests/e2e/e2e.spec.ts
@@ -31,3 +31,32 @@ test('test v2 ui part 2', async ({ page }) => {
   await page.getByRole('button', { name: 'Generate File' }).click()
   await page.getByRole('button', { name: 'Download PDF' }).click()
 })
+
+test('test books retained in basket on back button to languages and then forward', async ({
+  page
+}) => {
+  await page.goto('http://localhost:8001/')
+  await page.goto('http://localhost:8001/#/languages')
+  await page.getByText('Amharic').click()
+  await page.getByRole('button', { name: 'Heart' }).click()
+  await page.getByPlaceholder('Search Languages').click()
+  await page.getByPlaceholder('Search Languages').fill('agn')
+  await page.getByLabel('Agni Bona any-x-agnibona').check()
+  await page.getByRole('button', { name: 'Next' }).click()
+  await page.getByText('Philemon').click()
+  await page.getByText('Philemon').nth(1).click()
+  await page
+    .locator('div')
+    .filter({ hasText: /^Amharic\(am\)$/ })
+    .first()
+    .click()
+  await page.locator('.flex-shrink-0 > div:nth-child(4)').click()
+  await page.getByRole('link', { name: 'Languages' }).click()
+  await page.getByRole('button', { name: 'Next' }).click()
+  await page.getByText('Philemon').nth(1).click()
+  await page
+    .locator('div')
+    .filter({ hasText: /^Amharic\(am\)$/ })
+    .first()
+    .click()
+})
