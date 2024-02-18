d773e5e8faa9c8a7da4ce5f2cba718940bd8c9ae
linearcombination <4829djaskdfj@gmail.com>
2024-02-17 17:12:33 -0800

    Reorg some code in assembly logic

--- backend/document/domain/assembly_strategies/assembly_strategies_book_then_lang_by_chapter.py
+++ backend/document/domain/assembly_strategies/assembly_strategies_book_then_lang_by_chapter.py
@@ -28,6 +28,203 @@ from document.utils.number_utils import is_even
 logger = settings.logger(__name__)
 
 
+def assemble_usfm_by_chapter(
+    usfm_book_content_units: Sequence[USFMBook],
+    tn_book_content_units: Sequence[TNBook],
+    tq_book_content_units: Sequence[TQBook],
+    tw_book_content_units: Sequence[TWBook],
+    bc_book_content_units: Sequence[BCBook],
+    end_of_chapter_html: str = settings.END_OF_CHAPTER_HTML,
+    hr: str = "<hr/>",
+    close_direction_html: str = "</div>",
+) -> Iterable[HtmlContent]:
+    """
+    Construct the HTML wherein at least one USFM resource exists, one column
+    layout.
+    """
+
+    ldebug = logger.debug
+    lexception = logger.exception
+
+    def sort_key(resource: BookContent) -> str:
+        return resource.lang_code
+
+    usfm_book_content_units = sorted(usfm_book_content_units, key=sort_key)
+    tn_book_content_units = sorted(tn_book_content_units, key=sort_key)
+    tq_book_content_units = sorted(tq_book_content_units, key=sort_key)
+    bc_book_content_units = sorted(bc_book_content_units, key=sort_key)
+    # Add book intros for each tn_book_content_unit
+    for tn_book_content_unit in tn_book_content_units:
+        # if tn_book_content_unit.book_intro:
+        yield from language_direction_html(tn_book_content_unit)
+        book_intro_ = "".join(list(book_intro(tn_book_content_unit)))
+        book_intro_adj = adjust_book_intro_headings(book_intro_)
+        yield book_intro_adj
+        yield close_direction_html
+    for bc_book_content_unit in bc_book_content_units:
+        yield from book_intro(bc_book_content_unit)
+    # Use the usfm_book_content_unit that has the most chapters as a
+    # chapter_num pump to realize the most amount of content displayed
+    # to the user.
+    usfm_with_most_chapters = max(
+        usfm_book_content_units,
+        key=lambda usfm_book_content_unit: usfm_book_content_unit.chapters.keys(),
+    )
+    yield book_title(usfm_with_most_chapters.book_code)
+    for chapter_num, chapter in usfm_with_most_chapters.chapters.items():
+        for usfm_book_content_unit in usfm_book_content_units:
+            if chapter_num in usfm_book_content_unit.chapters:
+                yield from language_direction_html(usfm_book_content_unit)
+                yield chapter_content_sans_footnotes(
+                    usfm_book_content_unit.chapters[chapter_num].content
+                )
+                yield hr
+                yield from chapter_footnotes(
+                    usfm_book_content_unit.chapters[chapter_num]
+                )
+                yield close_direction_html
+        for tn_book_content_unit2 in tn_book_content_units:
+            yield from language_direction_html(tn_book_content_unit2)
+            yield from chapter_intro(tn_book_content_unit2, chapter_num)
+            yield close_direction_html
+        for bc_book_content_unit in bc_book_content_units:
+            yield from chapter_commentary(bc_book_content_unit, chapter_num)
+        # Add the interleaved tn notes
+        tn_verses = None
+        for tn_book_content_unit3 in tn_book_content_units:
+            tn_verses = list(tn_chapter_verses(tn_book_content_unit3, chapter_num))
+            # if tn_verses:
+            yield from language_direction_html(tn_book_content_unit3)
+            yield "".join(tn_verses)
+            yield close_direction_html
+        tq_verses = None
+        for tq_book_content_unit in tq_book_content_units:
+            tq_verses = list(tq_chapter_verses(tq_book_content_unit, chapter_num))
+            # if tq_verses:
+            yield from language_direction_html(tq_book_content_unit)
+            yield "".join(tq_verses)
+            yield close_direction_html
+        yield end_of_chapter_html
+
+
+def assemble_tn_by_chapter(
+    usfm_book_content_units: Sequence[USFMBook],
+    tn_book_content_units: Sequence[TNBook],
+    tq_book_content_units: Sequence[TQBook],
+    tw_book_content_units: Sequence[TWBook],
+    bc_book_content_units: Sequence[BCBook],
+    end_of_chapter_html: str = settings.END_OF_CHAPTER_HTML,
+    close_direction_html: str = "</div>",
+) -> Iterable[HtmlContent]:
+    """
+    Construct the HTML for a 'by chapter' strategy wherein at least
+    tn_book_content_units exists.
+    """
+
+    def sort_key(resource: BookContent) -> str:
+        return resource.lang_code
+
+    tn_book_content_units = sorted(tn_book_content_units, key=sort_key)
+    tq_book_content_units = sorted(tq_book_content_units, key=sort_key)
+    bc_book_content_units = sorted(bc_book_content_units, key=sort_key)
+
+    # Add book intros for each tn_book_content_unit
+    for tn_book_content_unit in tn_book_content_units:
+        yield from language_direction_html(tn_book_content_unit)
+        book_intro_ = book_intro(tn_book_content_unit)
+        book_intro_adj = adjust_book_intro_headings("".join(book_intro_))
+        yield book_intro_adj
+        yield close_direction_html
+    for bc_book_content_unit in bc_book_content_units:
+        yield from book_intro(bc_book_content_unit)
+    # Use the tn_book_content_unit that has the most chapters as a
+    # chapter_num pump to realize the most amount of content displayed
+    # to user.
+    tn_with_most_chapters = max(
+        tn_book_content_units,
+        key=lambda tn_book_content_unit: tn_book_content_unit.chapters.keys(),
+    )
+    for chapter_num in tn_with_most_chapters.chapters.keys():
+        yield HtmlContent("Chapter {}".format(chapter_num))
+        # Add chapter intro for each language
+        for tn_book_content_unit in tn_book_content_units:
+            yield from language_direction_html(tn_book_content_unit)
+            yield from chapter_intro(tn_book_content_unit, chapter_num)
+            yield close_direction_html
+        for bc_book_content_unit in bc_book_content_units:
+            yield from chapter_commentary(bc_book_content_unit, chapter_num)
+        # Add the interleaved tn notes
+        for tn_book_content_unit in tn_book_content_units:
+            tn_verses = list(tn_chapter_verses(tn_book_content_unit, chapter_num))
+            yield from language_direction_html(tn_book_content_unit)
+            yield "".join(tn_verses)
+            yield close_direction_html
+        # Add the interleaved tq questions
+        for tq_book_content_unit in tq_book_content_units:
+            tq_verses = list(tq_chapter_verses(tq_book_content_unit, chapter_num))
+            yield from language_direction_html(tq_book_content_unit)
+            yield "".join(tq_verses)
+            yield close_direction_html
+        yield end_of_chapter_html
+
+
+def assemble_tq_by_chapter(
+    usfm_book_content_units: Sequence[USFMBook],
+    tn_book_content_units: Sequence[TNBook],
+    tq_book_content_units: Sequence[TQBook],
+    tw_book_content_units: Sequence[TWBook],
+    bc_book_content_units: Sequence[BCBook],
+    end_of_chapter_html: str = settings.END_OF_CHAPTER_HTML,
+    close_direction_html: str = "</div>",
+) -> Iterable[HtmlContent]:
+    """
+    Construct the HTML for a 'by chapter' strategy wherein at least
+    tq_book_content_units exists.
+    """
+
+    def sort_key(resource: BookContent) -> str:
+        return resource.lang_code
+
+    tq_book_content_units = sorted(tq_book_content_units, key=sort_key)
+    bc_book_content_units = sorted(bc_book_content_units, key=sort_key)
+    # Use the tq_book_content_unit that has the most chapters as a
+    # chapter_num pump to realize the most amount of content displayed to user.
+    tq_with_most_chapters = max(
+        tq_book_content_units,
+        key=lambda tq_book_content_unit: tq_book_content_unit.chapters.keys(),
+    )
+    for chapter_num in tq_with_most_chapters.chapters.keys():
+        yield HtmlContent("Chapter {}".format(chapter_num))
+        for bc_book_content_unit in bc_book_content_units:
+            yield from chapter_commentary(bc_book_content_unit, chapter_num)
+        # Add the interleaved tq questions
+        for tq_book_content_unit in tq_book_content_units:
+            tq_verses = list(tq_chapter_verses(tq_book_content_unit, chapter_num))
+            yield from language_direction_html(tq_book_content_unit)
+            yield "".join(tq_verses)
+            yield close_direction_html
+        yield end_of_chapter_html
+
+
+def assemble_tw_by_chapter(
+    usfm_book_content_units: Sequence[USFMBook],
+    tn_book_content_units: Sequence[TNBook],
+    tq_book_content_units: Sequence[TQBook],
+    tw_book_content_units: Sequence[TWBook],
+    bc_book_content_units: Sequence[BCBook],
+    end_of_chapter_html: str = settings.END_OF_CHAPTER_HTML,
+) -> Iterable[HtmlContent]:
+    def sort_key(resource: BookContent) -> str:
+        return resource.lang_code
+
+    bc_book_content_units = sorted(bc_book_content_units, key=sort_key)
+    for bc_book_content_unit in bc_book_content_units:
+        yield from book_intro(bc_book_content_unit)
+        for chapter_num, chapter in bc_book_content_unit.chapters.items():
+            yield from chapter_commentary(bc_book_content_unit, chapter_num)
+            yield end_of_chapter_html
+
+
 def assemble_usfm_by_chapter_2c_sl_sr(
     usfm_book_content_units: Sequence[USFMBook],
     tn_book_content_units: Sequence[TNBook],
@@ -256,200 +453,3 @@ def assemble_usfm_by_chapter_2c_sl_sr(
                 yield html_column_end
         yield html_row_end
         yield html_row_end
-
-
-def assemble_usfm_by_chapter(
-    usfm_book_content_units: Sequence[USFMBook],
-    tn_book_content_units: Sequence[TNBook],
-    tq_book_content_units: Sequence[TQBook],
-    tw_book_content_units: Sequence[TWBook],
-    bc_book_content_units: Sequence[BCBook],
-    end_of_chapter_html: str = settings.END_OF_CHAPTER_HTML,
-    hr: str = "<hr/>",
-    close_direction_html: str = "</div>",
-) -> Iterable[HtmlContent]:
-    """
-    Construct the HTML wherein at least one USFM resource exists, one column
-    layout.
-    """
-
-    ldebug = logger.debug
-    lexception = logger.exception
-
-    def sort_key(resource: BookContent) -> str:
-        return resource.lang_code
-
-    usfm_book_content_units = sorted(usfm_book_content_units, key=sort_key)
-    tn_book_content_units = sorted(tn_book_content_units, key=sort_key)
-    tq_book_content_units = sorted(tq_book_content_units, key=sort_key)
-    bc_book_content_units = sorted(bc_book_content_units, key=sort_key)
-    # Add book intros for each tn_book_content_unit
-    for tn_book_content_unit in tn_book_content_units:
-        # if tn_book_content_unit.book_intro:
-        yield from language_direction_html(tn_book_content_unit)
-        book_intro_ = "".join(list(book_intro(tn_book_content_unit)))
-        book_intro_adj = adjust_book_intro_headings(book_intro_)
-        yield book_intro_adj
-        yield close_direction_html
-    for bc_book_content_unit in bc_book_content_units:
-        yield from book_intro(bc_book_content_unit)
-    # Use the usfm_book_content_unit that has the most chapters as a
-    # chapter_num pump to realize the most amount of content displayed
-    # to the user.
-    usfm_with_most_chapters = max(
-        usfm_book_content_units,
-        key=lambda usfm_book_content_unit: usfm_book_content_unit.chapters.keys(),
-    )
-    yield book_title(usfm_with_most_chapters.book_code)
-    for chapter_num, chapter in usfm_with_most_chapters.chapters.items():
-        for usfm_book_content_unit in usfm_book_content_units:
-            if chapter_num in usfm_book_content_unit.chapters:
-                yield from language_direction_html(usfm_book_content_unit)
-                yield chapter_content_sans_footnotes(
-                    usfm_book_content_unit.chapters[chapter_num].content
-                )
-                yield hr
-                yield from chapter_footnotes(
-                    usfm_book_content_unit.chapters[chapter_num]
-                )
-                yield close_direction_html
-        for tn_book_content_unit2 in tn_book_content_units:
-            yield from language_direction_html(tn_book_content_unit2)
-            yield from chapter_intro(tn_book_content_unit2, chapter_num)
-            yield close_direction_html
-        for bc_book_content_unit in bc_book_content_units:
-            yield from chapter_commentary(bc_book_content_unit, chapter_num)
-        # Add the interleaved tn notes
-        tn_verses = None
-        for tn_book_content_unit3 in tn_book_content_units:
-            tn_verses = list(tn_chapter_verses(tn_book_content_unit3, chapter_num))
-            # if tn_verses:
-            yield from language_direction_html(tn_book_content_unit3)
-            yield "".join(tn_verses)
-            yield close_direction_html
-        tq_verses = None
-        for tq_book_content_unit in tq_book_content_units:
-            tq_verses = list(tq_chapter_verses(tq_book_content_unit, chapter_num))
-            # if tq_verses:
-            yield from language_direction_html(tq_book_content_unit)
-            yield "".join(tq_verses)
-            yield close_direction_html
-        yield end_of_chapter_html
-
-
-def assemble_tn_by_chapter(
-    usfm_book_content_units: Sequence[USFMBook],
-    tn_book_content_units: Sequence[TNBook],
-    tq_book_content_units: Sequence[TQBook],
-    tw_book_content_units: Sequence[TWBook],
-    bc_book_content_units: Sequence[BCBook],
-    end_of_chapter_html: str = settings.END_OF_CHAPTER_HTML,
-    close_direction_html: str = "</div>",
-) -> Iterable[HtmlContent]:
-    """
-    Construct the HTML for a 'by chapter' strategy wherein at least
-    tn_book_content_units exists.
-    """
-
-    def sort_key(resource: BookContent) -> str:
-        return resource.lang_code
-
-    tn_book_content_units = sorted(tn_book_content_units, key=sort_key)
-    tq_book_content_units = sorted(tq_book_content_units, key=sort_key)
-    bc_book_content_units = sorted(bc_book_content_units, key=sort_key)
-
-    # Add book intros for each tn_book_content_unit
-    for tn_book_content_unit in tn_book_content_units:
-        yield from language_direction_html(tn_book_content_unit)
-        book_intro_ = book_intro(tn_book_content_unit)
-        book_intro_adj = adjust_book_intro_headings("".join(book_intro_))
-        yield book_intro_adj
-        yield close_direction_html
-    for bc_book_content_unit in bc_book_content_units:
-        yield from book_intro(bc_book_content_unit)
-    # Use the tn_book_content_unit that has the most chapters as a
-    # chapter_num pump to realize the most amount of content displayed
-    # to user.
-    tn_with_most_chapters = max(
-        tn_book_content_units,
-        key=lambda tn_book_content_unit: tn_book_content_unit.chapters.keys(),
-    )
-    for chapter_num in tn_with_most_chapters.chapters.keys():
-        yield HtmlContent("Chapter {}".format(chapter_num))
-        # Add chapter intro for each language
-        for tn_book_content_unit in tn_book_content_units:
-            yield from language_direction_html(tn_book_content_unit)
-            yield from chapter_intro(tn_book_content_unit, chapter_num)
-            yield close_direction_html
-        for bc_book_content_unit in bc_book_content_units:
-            yield from chapter_commentary(bc_book_content_unit, chapter_num)
-        # Add the interleaved tn notes
-        for tn_book_content_unit in tn_book_content_units:
-            tn_verses = list(tn_chapter_verses(tn_book_content_unit, chapter_num))
-            yield from language_direction_html(tn_book_content_unit)
-            yield "".join(tn_verses)
-            yield close_direction_html
-        # Add the interleaved tq questions
-        for tq_book_content_unit in tq_book_content_units:
-            tq_verses = list(tq_chapter_verses(tq_book_content_unit, chapter_num))
-            yield from language_direction_html(tq_book_content_unit)
-            yield "".join(tq_verses)
-            yield close_direction_html
-        yield end_of_chapter_html
-
-
-def assemble_tq_by_chapter(
-    usfm_book_content_units: Sequence[USFMBook],
-    tn_book_content_units: Sequence[TNBook],
-    tq_book_content_units: Sequence[TQBook],
-    tw_book_content_units: Sequence[TWBook],
-    bc_book_content_units: Sequence[BCBook],
-    end_of_chapter_html: str = settings.END_OF_CHAPTER_HTML,
-    close_direction_html: str = "</div>",
-) -> Iterable[HtmlContent]:
-    """
-    Construct the HTML for a 'by chapter' strategy wherein at least
-    tq_book_content_units exists.
-    """
-
-    def sort_key(resource: BookContent) -> str:
-        return resource.lang_code
-
-    tq_book_content_units = sorted(tq_book_content_units, key=sort_key)
-    bc_book_content_units = sorted(bc_book_content_units, key=sort_key)
-    # Use the tq_book_content_unit that has the most chapters as a
-    # chapter_num pump to realize the most amount of content displayed to user.
-    tq_with_most_chapters = max(
-        tq_book_content_units,
-        key=lambda tq_book_content_unit: tq_book_content_unit.chapters.keys(),
-    )
-    for chapter_num in tq_with_most_chapters.chapters.keys():
-        yield HtmlContent("Chapter {}".format(chapter_num))
-        for bc_book_content_unit in bc_book_content_units:
-            yield from chapter_commentary(bc_book_content_unit, chapter_num)
-        # Add the interleaved tq questions
-        for tq_book_content_unit in tq_book_content_units:
-            tq_verses = list(tq_chapter_verses(tq_book_content_unit, chapter_num))
-            yield from language_direction_html(tq_book_content_unit)
-            yield "".join(tq_verses)
-            yield close_direction_html
-        yield end_of_chapter_html
-
-
-def assemble_tw_by_chapter(
-    usfm_book_content_units: Sequence[USFMBook],
-    tn_book_content_units: Sequence[TNBook],
-    tq_book_content_units: Sequence[TQBook],
-    tw_book_content_units: Sequence[TWBook],
-    bc_book_content_units: Sequence[BCBook],
-    end_of_chapter_html: str = settings.END_OF_CHAPTER_HTML,
-) -> Iterable[HtmlContent]:
-    def sort_key(resource: BookContent) -> str:
-        return resource.lang_code
-
-    bc_book_content_units = sorted(bc_book_content_units, key=sort_key)
-    for bc_book_content_unit in bc_book_content_units:
-        yield from book_intro(bc_book_content_unit)
-        for chapter_num, chapter in bc_book_content_unit.chapters.items():
-            yield from chapter_commentary(bc_book_content_unit, chapter_num)
-            yield end_of_chapter_html
